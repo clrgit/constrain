c6f0d486458ba165f9ebbd4efca8c1a23fc09240
Claus Rasmussen <claus.l.rasmussen@gmail.com>
2021-05-13 02:36:32 +0200

    Gem package import

---
+++ lib/constrain.rb
@@ -0,0 +1,6 @@
+require "constrain/version"
+
+module Constrain
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/constrain_spec.rb
@@ -0,0 +1,7 @@
+describe Constrain do
+  it 'has a version number' do
+    expect(Constrain::VERSION).not_to be_nil
+  end
+
+  it 'does something useful'
+end
